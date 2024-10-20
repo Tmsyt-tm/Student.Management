d6025c50ccb6afd2944c57f484cde1256636861a
Tmsyt-tm <kgkyq494@yahoo.co.jp>
2024-10-20 16:59:52 +0900

    20241113のスタンダード授業にて  //MyBatis 	implementation'org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.3' 	の機能を追加

---
+++ src/main/java/raisentech/Student/Management/Application.java
@@ -0,0 +1,30 @@
+package raisentech.Student.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+	private  String name ="Enami Kouzi";
+	private  String age ="37";
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/studentInfo")
+	public  String  getStudentInfo()  {
+		return name + " " + age +"歳";
+	}
+
+	@PostMapping("/studentInfo")
+	public  void  setStudentInfo(String name,String age){
+		this.name = name;
+		this.age = age;
+	}
+}
+
+
