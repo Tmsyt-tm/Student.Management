c637dd4a7c6f59a6f45d77f03213b740f6f6ff5a
Tmsyt-tm <kgkyq494@yahoo.co.jp>
2024-11-27 19:42:30 +0900

    202411127のスタンダード授業にて データベースへ接続し名前で検索をして情報を取得する機能を実装した データベースへ接続しデータの更新と削除の機能を実装した データベースを全件取得する機能を実装した

--- src/main/java/raisentech/Student/Management/StudentRepository.java
+++ src/main/java/raisentech/Student/Management/StudentRepository.java
@@ -1,5 +1,6 @@
 package raisentech.Student.Management;
 
+import java.util.List;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -9,18 +10,25 @@ import org.apache.ibatis.annotations.Update;
 @Mapper
 public interface StudentRepository {
 
+   //検索機能 一行取得
    @Select("SELECT * FROM student WHERE name = #{name}")
    Student searchByName(String name);
 
+  //登録機能
    @Insert("INSERT student values(#{name},#{age})")
    void  registerStudent(String name, int age);
 
+   //更新機能
    @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
    void updateStudent(String name , int age);
 
+  //削除機能
    @Delete("DELETE FROM student WHERE name = #{name}")
    void  deleteStudent(String name);
 
+   //データベース全取得機能
+   @Select("SELECT * FROM student")
+   List<Student> findAll();
 }
 
 
