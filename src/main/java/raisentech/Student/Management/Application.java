60804daf22e9ad6330e57b7468ca6241b408d7d2
Tmsyt-tm <kgkyq494@yahoo.co.jp>
2024-11-27 19:55:45 +0900

    202411127のスタンダード授業にて データベースへ接続し名前で検索をして情報を取得する機能を実装した データベースへ接続しデータの更新と削除の機能を実装した データベースを全件取得する機能を実装した

--- src/main/java/raisentech/Student/Management/Application.java
+++ src/main/java/raisentech/Student/Management/Application.java
@@ -1,5 +1,6 @@
 package raisentech.Student.Management;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
 
 	@Autowired
-	private  StudentRepository repository;
+	private StudentRepository repository;
 
 
 	public static void main(String[] args) {
@@ -24,26 +25,28 @@ public class Application {
 	}
 
 	@GetMapping("/student")
-	public  String  getStudentInfo(@RequestParam String name)  {
+	public String getStudentInfo(@RequestParam String name) {
 		Student student = repository.searchByName(name);
-		return student.getName() + " " + student.getAge() +"歳";
+		return student.getName() + " " + student.getAge() + "歳";
 	}
 
 	@PostMapping("/student")
-	public  void  registerStudent(String name, int age){
- repository.registerStudent(name , age);
+	public void registerStudent(String name, int age) {
+		repository.registerStudent(name, age);
 	}
 
-	@PatchMapping ("/student")
-	public  void  updateStudent(String name , int age){
+	@PatchMapping("/student")
+	public void updateStudent(String name, int age) {
 		repository.updateStudent(name, age);
 	}
 
 	@DeleteMapping("/student")
-	public  void deleteStudent(String name){
-		repository .deleteStudent(name);
+	public void deleteStudent(String name) {
+		repository.deleteStudent(name);
 	}
 
+	@GetMapping("/students")
+	public List<Student> getAllStudents() {
+		return repository.findAll();
+	}
 }
-
-
