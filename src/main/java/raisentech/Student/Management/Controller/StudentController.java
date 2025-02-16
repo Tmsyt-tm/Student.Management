e02082665f90eb8731df51964df811f94d425847
Tmsyt-tm <kgkyq494@yahoo.co.jp>
2025-02-16 17:30:54 +0900

    thymeleafを使いhtml画面表示を実装しました

--- src/main/java/raisentech/Student/Management/Controller/StudentController.java
+++ src/main/java/raisentech/Student/Management/Controller/StudentController.java
@@ -1,18 +1,19 @@
 package raisentech.Student.Management.Controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.model.IAttribute;
 import raisentech.Student.Management.Controller.converter.StudentConverter;
 import raisentech.Student.Management.data.Student;
 import raisentech.Student.Management.data.StudentsCourses;
 import raisentech.Student.Management.domain.StudentDetail;
 import raisentech.Student.Management.service.StudentService;
 
-@RestController
+@Controller
 public class StudentController {
 
   private StudentService service;
@@ -26,28 +27,12 @@ public class StudentController {
   }
 
   @GetMapping("/studentList")
-  public List<StudentDetail> getStudentList() {
+  public String getStudentList(Model model) {
     List<Student> students = service.searchStudentList();
     List<StudentsCourses> studentsCourses = service.searchStudentsCourseList();
 
-    return convertStudentDetails(students, studentsCourses);
-  }
-
-  private List<StudentDetail> convertStudentDetails(List<Student> students,
-      List<StudentsCourses> studentsCourses) {
-    List<StudentDetail> studentDetails = new ArrayList<>();
-    students.forEach(student -> {
-      StudentDetail studentDetail = new StudentDetail();
-      studentDetail.setStudent(student);
-
-      List<StudentsCourses> covertStudentCourses = studentsCourses.stream()
-          .filter(studentCourse -> student.getId().equals(studentCourse.getStudentId()))
-          .collect(Collectors.toList());
-
-      studentDetail.setStudentsCourses(covertStudentCourses);
-      studentDetails.add(studentDetail);
-    });
-    return studentDetails;
+    model.addAttribute("studentList", converter.convertStudentDetails(students, studentsCourses));
+    return "studentList";
   }
 
   @GetMapping("/studentCourseList")
--- src/main/java/raisentech/Student/Management/Controller/converter/StudentConverter.java
+++ src/main/java/raisentech/Student/Management/Controller/converter/StudentConverter.java
@@ -1,8 +1,30 @@
 package raisentech.Student.Management.Controller.converter;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
+import raisentech.Student.Management.data.Student;
+import raisentech.Student.Management.data.StudentsCourses;
+import raisentech.Student.Management.domain.StudentDetail;
 
 @Component
 public class StudentConverter {
 
+  public List<StudentDetail> convertStudentDetails(List<Student> students,
+      List<StudentsCourses> studentsCourses) {
+    List<StudentDetail> studentDetails = new ArrayList<>();
+    students.forEach(student -> {
+      StudentDetail studentDetail = new StudentDetail();
+      studentDetail.setStudent(student);
+
+      List<StudentsCourses> covertStudentCourses = studentsCourses.stream()
+          .filter(studentCourse -> student.getId().equals(studentCourse.getStudentId()))
+          .collect(Collectors.toList());
+
+      studentDetail.setStudentsCourses(covertStudentCourses);
+      studentDetails.add(studentDetail);
+    });
+    return studentDetails;
+  }
 }
